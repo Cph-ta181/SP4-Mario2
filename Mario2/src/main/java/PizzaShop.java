9d5858c032e9680744d009e9f288d41df04e8dbb
Cph-Ta181 <cph-ta181@cphbusiness.dk>
2020-11-23 14:56:39 +0100

    asd

--- Mario2/src/main/java/PizzaShop.java
+++ Mario2/src/main/java/PizzaShop.java
@@ -1,3 +1,27 @@
-public class PizzaShop {
+import java.sql.*;
+import java.util.ArrayList;
 
+public class PizzaShop {
+    private Menu menu;
+
+    public PizzaShop(Menu menu){
+        this.menu = menu;
+    }
+
+    public ArrayList<Order> getOrders(){
+        ArrayList<Order> orderArr = new ArrayList<Order>();
+        Connection con = null;
+
+        try{
+            con = DriverManager.getConnection("jdbc:mysql://localhost/pizzaShop", "root","root123");
+            PreparedStatement preSta = con.prepareStatement("SELECT COUNT(*) FROM activeOrder;");
+            ResultSet res = preSta.executeQuery();
+            System.out.println(res);
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+        return  orderArr;
+    }
 }
