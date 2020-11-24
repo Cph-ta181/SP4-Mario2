e757a2f02d338a201dfb0fe0aa87545d9f3cf1e2
DanielD99 <71074562+DanielD99@users.noreply.github.com>
2020-11-24 13:49:27 +0100

    Update Menu.java

--- Mario2/src/main/java/Menu.java
+++ Mario2/src/main/java/Menu.java
@@ -1,2 +1,51 @@
-public class Menu {
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MenuKort {
+    private ArrayList<Pizza> pizzas;
+
+    public Menu(String filename) {
+        pizzas = new ArrayList<Pizza>();
+        File file = new File(filename);
+        try {
+            FileReader fr = new FileReader(file);
+            BufferedReader br = new BufferedReader(fr);
+            String line = "";
+            int counter = 1;
+            Pizza pizza;
+            while((line = br.readLine()) != null) {
+                String[] lineArr = line.split(",");
+                pizza = new Pizza (lineArr[2], Integer.parseInt(lineArr[0]), Integer.parseInt(lineArr[1]));
+                pizzas.add(pizza);
+                counter++;
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+    public List<Pizza> getPizzas() {
+        return pizzas;
+    }
+
+    public void addPizza(Pizza pizzaToAdd){
+        pizzas.add(pizzaToAdd);
+    }
+
+    public void removePizza(Pizza pizzaToRemove) {
+        pizzas.remove(pizzaToRemove);
+    }
+
+    @Override
+    public String toString() {
+        String tempString = "";
+        for (Pizza pizza:pizzas) {
+            tempString+=pizza + "\n";
+        }
+        return tempString;
+    }
 }
